6a7d3339b2f03b52b5f226d2649f8d2bed496277
Kedith <204398694+KedithJW@users.noreply.github.com>
2025-11-24 20:44:42 -0800

    add

--- src/lab5/BorrowingService.java
+++ src/lab5/BorrowingService.java
@@ -1,5 +1,18 @@
 package lab5;
 
-public class BorrowingService {
+public class BorrowingService implements BorrowingServiceAPI{
+
+	@Override
+	public boolean borrowBook(Member member, Book book) {
+		member.borrowBook(book);
+		return false;
+	}
+
+	@Override
+	public boolean returnBook(Member member, Book book) {
+		return member.returnBook(book);
+		
+		
+	}
 
 }
