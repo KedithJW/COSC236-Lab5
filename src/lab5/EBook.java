a8c2ad4352094672f9aa209f481d537a1df10f2b
Chadgit <kenzforbes98@gmail.com>
2025-11-25 18:32:33 -0800

    Create EBook.java

---
+++ src/lab5/EBook.java
@@ -0,0 +1,31 @@
+public class EBook implements Book {
+
+    private String title;
+    private boolean isAvailable = true;
+
+    public EBook(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    @Override
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "EBook: " + title;
+    }
+}
+
+
