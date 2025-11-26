09052d0e08acf049b99ea52e97d2b1fd156e83df
Chadgit <kenzforbes98@gmail.com>
2025-11-25 18:35:01 -0800

    Create AudioBook.java

---
+++ src/lab5/AudioBook.java
@@ -0,0 +1,29 @@
+public class AudioBook implements Book {
+
+    private String title;
+    private boolean isAvailable = true;
+
+    public AudioBook(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    @Override
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "AudioBook: " + title;
+    }
+}
